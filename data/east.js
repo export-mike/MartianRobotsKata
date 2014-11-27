3bf15d32eaa4cea489938174c80212e9c3abee57
mike james <mike.james@redant.com>
2014-11-27 23:40:48 +0000

    Green light - tests passing.

--- data/east.js
+++ data/east.js
@@ -1,6 +1,15 @@
-function East(){
-
+function East(South, North){
+	this.South = South;
+	this.North = North;
 }
+
+East.prototype.turnLeft = function(){
+	return new this.South();
+};
+
+East.prototype.turnRight = function(){
+	return new this.North();
+};
 
 East.prototype.toString = function(){
 	return 'East';
