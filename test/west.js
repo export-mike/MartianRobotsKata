cd87cc8f1809bf27ea104a91fd0d38a993e9e332
mike james <mike.james@redant.com>
2014-12-01 22:29:11 +0000

    Redlight - moving method tests added failing

--- test/west.js
+++ test/west.js
@@ -10,28 +10,41 @@ var expect = Code.expect;
 
 var West = require('../data/west');
 
-suite('West',function(){
-	
+suite('West', function() {
+
 	var west;
-	
-	lab.beforeEach(function(done){
+
+	lab.beforeEach(function(done) {
 		west = new West();
 		done();
 	});
 
-	test('Returned string is west',function(done){		
+	test('Returned string is west', function(done) {
 		expect(west.toString()).to.equal('West');
 		done();
 	});
 
-	test('Return South when turning left', function(done){
+	test('Return South when turning left', function(done) {
 		expect(west.turnLeft()).to.equal('S');
 		done();
 	});
 
-	test('Return North when turning right', function(done){
+	test('Return North when turning right', function(done) {
 		expect(west.turnRight()).to.equal('N');
 		done();
 	});
 
+	test('Move Forward: New Location is x:1, y:1', function(done) {
+		var endPosition = west.moveForward(2, 1);
+		expect(endPosition.x).to.equal(1);
+		expect(endPosition.y).to.equal(1);
+		done();
+	});
+
+	test('Move Backward: New Location is x:2, y:1', function(done) {
+		var endPosition = west.moveBackward(1, 1);
+		expect(endPosition.x).to.equal(2);
+		expect(endPosition.y).to.equal(1);
+		done();
+	});
 });
