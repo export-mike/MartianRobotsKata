350fa335e129232c4be55d0300a3b8b999b47061
mike james <mike.james@redant.com>
2014-12-03 19:44:00 +0000

    Refactored East direction Type

--- data/east.js
+++ data/east.js
@@ -1,34 +1,3 @@
-// function East(Map){
-
-// 	this.moveForward = function(startX, startY){
-		
-// 		return Map.moveXForward(startX,startY);
-
-// 	};	
-
-// 	this.moveBackward = function(startX, startY){
-		
-// 		return Map.moveXBackward(startX,startY);
-
-// 	};
-// }
-
-// East.prototype.turnLeft = function(){
-// 	return 'N';
-// };
-
-// East.prototype.turnRight = function(){
-// 	return 'S';
-// };
-
-// East.prototype.facing = 'E';
-
-// East.prototype.toString = function(){
-// 	return 'East';
-// };
-
-// module.exports = East;
-
 var Base = require('./base-direction');
 
 function East(Map) {
