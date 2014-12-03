15fbd4c20169447d9ccfb0b308b419aac7b5992b
mike james <mike.james@redant.com>
2014-12-03 19:43:22 +0000

    GreenLight - for east moving tests

--- data/east.js
+++ data/east.js
@@ -1,30 +1,68 @@
-function East(Map){
+// function East(Map){
 
-	this.moveForward = function(startX, startY){
+// 	this.moveForward = function(startX, startY){
 		
-		return Map.moveXForward(startX,startY);
+// 		return Map.moveXForward(startX,startY);
 
-	};	
+// 	};	
 
-	this.moveBackward = function(startX, startY){
+// 	this.moveBackward = function(startX, startY){
 		
-		return Map.moveXBackward(startX,startY);
+// 		return Map.moveXBackward(startX,startY);
+
+// 	};
+// }
+
+// East.prototype.turnLeft = function(){
+// 	return 'N';
+// };
+
+// East.prototype.turnRight = function(){
+// 	return 'S';
+// };
+
+// East.prototype.facing = 'E';
+
+// East.prototype.toString = function(){
+// 	return 'East';
+// };
+
+// module.exports = East;
+
+var Base = require('./base-direction');
+
+function East(Map) {
+	var self = this;
+	var east = {};
+
+	east = Object.create(new Base());
+
+	east.moveForward = function(startX, startY) {
+
+		return east.move(startX, startY, Map.moveXForward, east.EAST_CHAR);
 
 	};
+
+	east.moveBackward = function(startX, startY) {
+		
+		return east.move(startX, startY, Map.moveXBackward, east.EAST_CHAR);
+	};
+
+	east.turnLeft = function() {
+		return east.NORTH_CHAR;
+	};
+
+	east.turnRight = function() {
+		return east.SOUTH_CHAR;
+	};
+
+	east.facing = east.EAST_CHAR;
+
+	east.toString = function() {
+		return 'East';
+	};
+
+	return east;
 }
-
-East.prototype.turnLeft = function(){
-	return 'N';
-};
-
-East.prototype.turnRight = function(){
-	return 'S';
-};
-
-East.prototype.facing = 'E';
-
-East.prototype.toString = function(){
-	return 'East';
-};
 
 module.exports = East;
