6d9ef72d39aad7485507b72f5831e102fbf5645e
mike james <mike.james@redant.com>
2014-12-03 22:55:10 +0000

    RedLight - input test failing

---
+++ test/robot-input-tests.js
@@ -0,0 +1,35 @@
+var Lab = require('lab');
+var Code = require('code');
+var lab = exports.lab = Lab.script();
+
+var suite = lab.suite;
+var test = lab.test;
+var before = lab.before;
+var after = lab.after;
+var expect = Code.expect;
+
+var Robot = require('../robot');
+var Map = require('../data/map');
+var East = require('../data/east');
+var North = require('../data/north');
+var South = require('../data/south');
+var West = require('../data/west');
+
+var path = require('path');
+
+var inputFile1 = require('fs').readFileSync(path.join(__dirname,'../input/input1.txt'), "utf8");
+var expectedFile1 = require('fs').readFileSync(path.join(__dirname,'../expected/expected1.txt'), "utf8");
+
+suite('Robot Criteria', function() {
+
+	test('Run Input File1 string provided', function(done) {
+		
+		var robotController = new RobotController(Robot);
+
+		var output = robotController.processInput(inputFile1);
+
+		expect(output).to.be.equal(expectedFile1);
+	});
+
+});
+
